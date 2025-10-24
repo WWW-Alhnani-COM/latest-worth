4a9782fd2e30862988c0d1628e731c636a8308e9
Mohammad Alhnani <145880506+WWW-Alhnani-COM@users.noreply.github.com>
2025-10-24 05:23:57 +0400

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -9,65 +9,7 @@ import {
   calculateMR_grandfatherHeir,
   calculateFR_grandmotherHeir,
   calculateMR_grandmotherHeir,
-  calculateSN_grandsonHeir,
-  calculateSN_granddaughterHeir,
-  calculateDR_grandsonHeir,
-  calculateDR_granddaughterHeir,
-  calculateBrotherHeir,
   calculateSisterHeir,
-  calculateMR_brotherHeir,
-  calculateMR_mother_sisterHeir,
-  calculateFR_brotherHeir,
-  calculateFR_sisterHeir,
-  calculateBR_boysHeir,
-  calculateSR_boysHeir,
-  calculateMR_BR_boysHeir,
-  calculateMR_SR_boysHeir,
-  calculateFR_BR_boysHeir,
-  calculateFR_SR_boysHeir,
-  calculateBR_girlsHeir,
-  calculateSR_girlsHeir,
-  calculateMR_BR_girlsHeir,
-  calculateMR_SR_girlsHeir,
-  calculateFR_BR_girlsHeir,
-  calculateFR_SR_girlsHeir,
-  calculateFR_uncleHeir,
-  calculateFR_auntHeir,
-  calculateMR_uncleHeir,
-  calculateMR_auntHeir,
-  calculateMR_uncle_motherHeir,
-  calculateFR_uncle_fatherHeir,
-  calculateMR_aunt_motherHeir,
-  calculateFR_aunt_fatherHeir,
-  calculateFR_uncle_father_AHeir,
-  calculateMR_uncle_mother_AHeir,
-  calculateFR_aunt_father_KHeir,
-  calculateMR_aunt_mother_KHeir,
-  calculateUncle_sons_AHeir,
-  calculateUncle_daughters_AHeir,
-  calculateAunt_sons_AHeir,
-  calculateAunt_daughters_AHeir,
-  calculateFR_uncle_sons_AHeir,
-  calculateMR_uncle_sons_AHeir,
-  calculateFR_uncle_daughter_AHeir,
-  calculateMR_uncle_daughter_AHeir,
-  calculateFR_aunt_sons_AHeir,
-  calculateMR_aunt_sons_AHeir,
-  calculateFR_aunt_daughter_AHeir,
-  calculateMR_aunt_daughter_AHeir,
-  calculateUncle_sons_KHeir,
-  calculateUncle_daughters_KHeir,
-  calculateAunt_sons_KHeir,
-  calculateAunt_daughters_KHeir,
-  calculateFR_uncle_sons_KHeir,
-  calculateMR_uncle_sons_KHeir,
-  calculateFR_uncle_daughter_KHeir,
-  calculateMR_uncle_daughter_KHeir,
-  calculateFR_aunt_sons_KHeir,
-  calculateMR_aunt_sons_KHeir,
-  calculateFR_aunt_daughter_KHeir,
-  calculateMR_aunt_daughter_KHeir,
-
 } from "./calculations.js";
 
 export function distribute(total = 10000, heirs) {
@@ -81,314 +23,215 @@ export function distribute(total = 10000, heirs) {
     heirCounts[heirType] = (heirCounts[heirType] || 0) + 1;
   }
 
-  // ترتيب الحساب حسب الأولوية الشرعية
-  const calculationOrder = [
-    'husband', 'wife',
-    'father', 'mother',
-    'FR_grandfather', 'MR_grandfather',
-    'FR_grandmother', 'MR_grandmother',
-    'sister',
-    'daughter', 'son'
-  ];
-
-  // حساب الورثة حسب الأولوية
-  for (const heirType of calculationOrder) {
+  // حساب الأنصبة الثابتة أولاً (الزوج، الزوجة، الأب، الأم، الجدات)
+  const fixedHeirs = ['husband', 'wife', 'father', 'mother', 'FR_grandmother', 'MR_grandmother', 'FR_grandfather'];
+  
+  for (const heirType of fixedHeirs) {
     const heirsOfType = Object.entries(heirs).filter(([type]) => 
       type.replace(/_[^_]+$/, '') === heirType
     );
 
     for (const [type, value] of heirsOfType) {
-      let percentage = 0;
-      let amount = 0;
-      let note = '';
-      
-      let params = {
-        heirCounts,
-        heirType,
-        percentage,
-        amount,
-        note,
-        type,
-        value,
-        heirs,
-        total: totalAmount,
-        results,
-        remainingAmount: totalAmount - getTotalAssignedAmount(results),
-        remainingPercentage: 100 - getTotalAssignedPercentage(results),
-      };
+      let calculationResult;
 
       switch (heirType) {
         case 'husband':
-          calculateHusbandHeir(params);
+          calculationResult = calculateHusbandHeir(heirs, totalAmount, heirCounts);
           break;
         case 'wife':
-          calculateWifeHeir(params);
+          calculationResult = calculateWifeHeir(heirs, totalAmount, heirCounts);
           break;
         case 'father':
-          calculateDadHeir(params);
+          calculationResult = calculateDadHeir(heirs, totalAmount, heirCounts);
           break;
         case 'mother':
-          calculateMomHeir(params);
-          break;
-        case 'son':
-          calculateSonHeir(params);
-          break;
-        case 'daughter':
-          calculateDaughterHeir(params);
-          break;
-        case 'sister':
-          calculateSisterHeir(params);
-          break;
-        case 'FR_grandfather':
-          calculateFR_grandfatherHeir(params);
-          break;
-        case 'MR_grandfather':
-          calculateMR_grandfatherHeir(params);
+          calculationResult = calculateMomHeir(heirs, totalAmount, heirCounts);
           break;
         case 'FR_grandmother':
-          calculateFR_grandmotherHeir(params);
+          calculationResult = calculateFR_grandmotherHeir(heirs, totalAmount, heirCounts);
           break;
         case 'MR_grandmother':
-          calculateMR_grandmotherHeir(params);
+          calculationResult = calculateMR_grandmotherHeir(heirs, totalAmount, heirCounts);
           break;
-        case 'SN_grandson':
-          calculateSN_grandsonHeir(params);
-          break;
-        case 'SN_granddaughter':
-          calculateSN_granddaughterHeir(params);
-          break;
-        case 'DR_grandson':
-          calculateDR_grandsonHeir(params);
-          break;
-        case 'DR_granddaughter':
-          calculateDR_granddaughterHeir(params);
-          break;
-        case 'brother':
-          calculateBrotherHeir(params);
-          break;
-        case 'MR_brother':
-          calculateMR_brotherHeir(params);
-          break;
-        case 'MR_mother_sister':
-          calculateMR_mother_sisterHeir(params);
-          break;
-        case 'FR_brother':
-          calculateFR_brotherHeir(params);
-          break;
-        case 'FR_sister':
-          calculateFR_sisterHeir(params);
-          break;
-        case 'BR_boys':
-          calculateBR_boysHeir(params);
-          break;
-        case 'SR_boys':
-          calculateSR_boysHeir(params);
-          break;
-        case 'MR_BR_boys':
-          calculateMR_BR_boysHeir(params);
-          break;
-        case 'MR_SR_boys':
-          calculateMR_SR_boysHeir(params);
-          break;
-        case 'FR_BR_boys':
-          calculateFR_BR_boysHeir(params);
-          break;
-        case 'FR_SR_boys':
-          calculateFR_SR_boysHeir(params);
-          break;
-        case 'BR_girls':
-          calculateBR_girlsHeir(params);
-          break;
-        case 'SR_girls':
-          calculateSR_girlsHeir(params);
-          break;
-        case 'MR_BR_girls':
-          calculateMR_BR_girlsHeir(params);
-          break;
-        case 'MR_SR_girls':
-          calculateMR_SR_girlsHeir(params);
-          break;
-        case 'FR_BR_girls':
-          calculateFR_BR_girlsHeir(params);
-          break;
-        case 'FR_SR_girls':
-          calculateFR_SR_girlsHeir(params);
-          break;
-        case 'FR_uncle':
-          calculateFR_uncleHeir(params);
-          break;
-        case 'FR_aunt':
-          calculateFR_auntHeir(params);
-          break;
-        case 'MR_uncle':
-          calculateMR_uncleHeir(params);
-          break;
-        case 'MR_aunt':
-          calculateMR_auntHeir(params);
-          break;
-        case 'MR_uncle_mother':
-          calculateMR_uncle_motherHeir(params);
-          break;
-        case 'FR_uncle_father':
-          calculateFR_uncle_fatherHeir(params);
-          break;
-        case 'MR_aunt_mother':
-          calculateMR_aunt_motherHeir(params);
-          break;
-        case 'FR_aunt_father':
-          calculateFR_aunt_fatherHeir(params);
-          break;
-        case 'FR_uncle_father_A':
-          calculateFR_uncle_father_AHeir(params);
-          break;
-        case 'MR_uncle_mother_A':
-          calculateMR_uncle_mother_AHeir(params);
-          break;
-        case 'FR_aunt_father_K':
-          calculateFR_aunt_father_KHeir(params);
-          break;
-        case 'MR_aunt_mother_K':
-          calculateMR_aunt_mother_KHeir(params);
-          break;
-        case 'uncle_sons_A':
-          calculateUncle_sons_AHeir(params);
-          break;
-        case 'uncle_daughters_A':
-          calculateUncle_daughters_AHeir(params);
-          break;
-        case 'aunt_sons_A':
-          calculateAunt_sons_AHeir(params);
-          break;
-        case 'aunt_daughters_A':
-          calculateAunt_daughters_AHeir(params);
-          break;
-        case 'FR_uncle_sons_A':
-          calculateFR_uncle_sons_AHeir(params);
-          break;
-        case 'MR_uncle_sons_A':
-          calculateMR_uncle_sons_AHeir(params);
-          break;
-        case 'FR_uncle_daughter_A':
-          calculateFR_uncle_daughter_AHeir(params);
-          break;
-        case 'MR_uncle_daughter_A':
-          calculateMR_uncle_daughter_AHeir(params);
-          break;
-        case 'FR_aunt_sons_A':
-          calculateFR_aunt_sons_AHeir(params);
-          break;
-        case 'MR_aunt_sons_A':
-          calculateMR_aunt_sons_AHeir(params);
-          break;
-        case 'FR_aunt_daughter_A':
-          calculateFR_aunt_daughter_AHeir(params);
-          break;
-        case 'MR_aunt_daughter_A':
-          calculateMR_aunt_daughter_AHeir(params);
-          break;
-        case 'uncle_sons_K':
-          calculateUncle_sons_KHeir(params);
-          break;
-        case 'uncle_daughters_K':
-          calculateUncle_daughters_KHeir(params);
-          break;
-        case 'aunt_sons_K':
-          calculateAunt_sons_KHeir(params);
-          break;
-        case 'aunt_daughters_K':
-          calculateAunt_daughters_KHeir(params);
-          break;
-        case 'FR_uncle_sons_K':
-          calculateFR_uncle_sons_KHeir(params);
-          break;
-        case 'MR_uncle_sons_K':
-          calculateMR_uncle_sons_KHeir(params);
-          break;
-        case 'FR_uncle_daughter_K':
-          calculateFR_uncle_daughter_KHeir(params);
-          break;
-        case 'MR_uncle_daughter_K':
-          calculateMR_uncle_daughter_KHeir(params);
-          break;
-        case 'FR_aunt_sons_K':
-          calculateFR_aunt_sons_KHeir(params);
-          break;
-        case 'MR_aunt_sons_K':
-          calculateMR_aunt_sons_KHeir(params);
-          break;
-        case 'FR_aunt_daughter_K':
-          calculateFR_aunt_daughter_KHeir(params);
-          break;
-        case 'MR_aunt_daughter_K':
-          calculateMR_aunt_daughter_KHeir(params);
+        case 'FR_grandfather':
+          calculationResult = calculateFR_grandfatherHeir(heirs, totalAmount, heirCounts);
           break;
         default:
+          calculationResult = { amount: '0.00', percentage: '0.00', note: 'لم يتم التنفيذ' };
+      }
+
+      results[type] = {
+        ...value,
+        ...calculationResult
+      };
+    }
+  }
+
+  // حساب مجموع الأنصبة الثابتة
+  const totalFixedShares = Object.values(results).reduce((sum, result) => {
+    return sum + parseFloat(result.amount || 0);
+  }, 0);
+
+  const remainingAmount = totalAmount - totalFixedShares;
+
+  // الآن حساب الأبناء والبنات والأخوات (الذين يعتمدون على الباقي)
+  const remainingHeirs = ['son', 'daughter', 'sister'];
+  
+  for (const heirType of remainingHeirs) {
+    const heirsOfType = Object.entries(heirs).filter(([type]) => 
+      type.replace(/_[^_]+$/, '') === heirType
+    );
+
+    if (heirsOfType.length > 0) {
+      const sonCount = heirCounts['son'] || 0;
+      const daughterCount = heirCounts['daughter'] || 0;
+      const sisterCount = heirCounts['sister'] || 0;
+
+      if (sonCount > 0 && daughterCount > 0) {
+        // حالة: للذكر مثل حظ الانثيين
+        const totalShares = sonCount * 2 + daughterCount;
+        const sharePerUnit = remainingAmount / totalShares;
+        
+        // توزيع على الأبناء
+        const sonHeirs = heirsOfType.filter(([type]) => type.startsWith('son_'));
+        for (const [type, value] of sonHeirs) {
+          results[type] = {
+            ...value,
+            amount: (sharePerUnit * 2).toFixed(2),
+            percentage: (((sharePerUnit * 2) / totalAmount) * 100).toFixed(2),
+            note: 'للذكر مثل حظ الانثيين'
+          };
+        }
+
+        // توزيع على البنات
+        const daughterHeirs = heirsOfType.filter(([type]) => type.startsWith('daughter_'));
+        for (const [type, value] of daughterHeirs) {
+          results[type] = {
+            ...value,
+            amount: sharePerUnit.toFixed(2),
+            percentage: ((sharePerUnit / totalAmount) * 100).toFixed(2),
+            note: 'للذكر مثل حظ الانثيين'
+          };
+        }
+
+      } else if (daughterCount >= 2 && sonCount === 0) {
+        // حالة: ابنتين فصاعدا - الثلثين
+        const daughtersTotalShare = totalAmount * (2/3);
+        const sharePerDaughter = daughtersTotalShare / daughterCount;
+        
+        for (const [type, value] of heirsOfType) {
+          results[type] = {
+            ...value,
+            amount: sharePerDaughter.toFixed(2),
+            percentage: ((sharePerDaughter / totalAmount) * 100).toFixed(2),
+            note: 'ثلثين فرض'
+          };
+        }
+
+      } else if (daughterCount === 1 && sonCount === 0) {
+        // حالة: بنت واحدة - النصف
+        for (const [type, value] of heirsOfType) {
+          const share = totalAmount * (1/2);
+          results[type] = {
+            ...value,
+            amount: share.toFixed(2),
+            percentage: ((share / totalAmount) * 100).toFixed(2),
+            note: 'نصف فرض'
+          };
+        }
+
+      } else if (sonCount > 0 && daughterCount === 0) {
+        // حالة: أبناء فقط - الباقي تعصيباً
+        const sharePerSon = remainingAmount / sonCount;
+        
+        for (const [type, value] of heirsOfType) {
+          results[type] = {
+            ...value,
+            amount: sharePerSon.toFixed(2),
+            percentage: ((sharePerSon / totalAmount) * 100).toFixed(2),
+            note: 'الباقي تعصيباً'
+          };
+        }
+
+      } else if (sisterCount > 0 && sonCount > 0) {
+        // حالة: أخت مع ابن - للذكر مثل حظ الانثيين
+        const totalShares = sonCount * 2 + sisterCount;
+        const sharePerUnit = remainingAmount / totalShares;
+        
+        // توزيع على الأخوات
+        for (const [type, value] of heirsOfType) {
+          results[type] = {
+            ...value,
+            amount: sharePerUnit.toFixed(2),
+            percentage: ((sharePerUnit / totalAmount) * 100).toFixed(2),
+            note: 'للذكر مثل حظ الانثيين مع الابن'
+          };
+        }
+      } else {
+        // الحالات الأخرى
+        for (const [type, value] of heirsOfType) {
           results[type] = {
             ...value,
             amount: '0.00',
             percentage: '0.00',
-            note: 'لم يتم تنفيذ حساب هذا الوارث بعد',
+            note: 'لا توجد معالجة مناسبة'
           };
-          break;
-      }
-    }
-  }
-
-  // معالجة المتبقي (حالات الرد)
-  handleRemainingDistribution(results, totalAmount);
-
-  return results;
-}
-
-// دالة مساعدة لحساب إجمالي المبالغ المخصصة
-function getTotalAssignedAmount(results) {
-  return Object.values(results).reduce((total, result) => {
-    return total + (parseFloat(result.amount) || 0);
-  }, 0);
-}
-
-// دالة مساعدة لحساب إجمالي النسب المخصصة
-function getTotalAssignedPercentage(results) {
-  return Object.values(results).reduce((total, result) => {
-    return total + (parseFloat(result.percentage) || 0);
-  }, 0);
-}
-
-// دالة محسنة لمعالجة المتبقي
-function handleRemainingDistribution(results, totalAmount) {
-  const totalAssigned = getTotalAssignedAmount(results);
-  const remaining = totalAmount - totalAssigned;
-
-  if (remaining > 0.01) { // تحمل خطأ بسيط في الحسابات العشرية
-    const eligibleHeirs = Object.entries(results)
-      .filter(([type, result]) => {
-        const amount = parseFloat(result.amount) || 0;
-        return amount > 0 && !type.includes('wife') && !type.includes('husband');
-      });
-
-    if (eligibleHeirs.length > 0) {
-      const sharePerHeir = remaining / eligibleHeirs.length;
-      
-      for (const [type, result] of eligibleHeirs) {
-        const currentAmount = parseFloat(result.amount) || 0;
-        results[type].amount = (currentAmount + sharePerHeir).toFixed(2);
-        results[type].percentage = ((parseFloat(results[type].amount) / totalAmount) * 100).toFixed(2);
-        
-        if (!results[type].note.includes('الرد')) {
-          results[type].note += ' مع الرد';
         }
       }
     }
   }
 
-  // التأكد من أن المجموع النهائي = المبلغ الكلي
-  const finalTotal = getTotalAssignedAmount(results);
+  // معالجة الرد إذا كان هناك متبقي
+  handleAlRadd(results, totalAmount);
+
+  return results;
+}
+
+// دالة معالجة الرد
+function handleAlRadd(results, totalAmount) {
+  const totalAssigned = Object.values(results).reduce((sum, result) => {
+    return sum + parseFloat(result.amount || 0);
+  }, 0);
+
+  const remaining = totalAmount - totalAssigned;
+
+  if (remaining > 0.01) {
+    // الورثة الذين يستفيدون من الرد (غير الزوج/الزوجة)
+    const eligibleHeirs = Object.entries(results).filter(([type, result]) => {
+      const amount = parseFloat(result.amount || 0);
+      return amount > 0 && 
+             !type.includes('wife') && 
+             !type.includes('husband') &&
+             !result.note.includes('تعصيب');
+    });
+
+    if (eligibleHeirs.length > 0) {
+      const sharePerHeir = remaining / eligibleHeirs.length;
+      
+      for (const [type, result] of eligibleHeirs) {
+        const currentAmount = parseFloat(result.amount || 0);
+        results[type].amount = (currentAmount + sharePerHeir).toFixed(2);
+        results[type].percentage = ((parseFloat(results[type].amount) / totalAmount) * 100).toFixed(2);
+        
+        if (!results[type].note.includes('الرد')) {
+          results[type].note += ' + الرد';
+        }
+      }
+    }
+  }
+
+  // التأكد من أن المجموع = 100%
+  const finalTotal = Object.values(results).reduce((sum, result) => {
+    return sum + parseFloat(result.amount || 0);
+  }, 0);
+
   if (Math.abs(finalTotal - totalAmount) > 0.01) {
     // تصحيح الفروق البسيطة
     const difference = totalAmount - finalTotal;
-    const firstHeir = Object.keys(results).find(key => parseFloat(results[key].amount) > 0);
+    const firstHeir = Object.keys(results).find(key => parseFloat(results[key].amount || 0) > 0);
     if (firstHeir) {
-      results[firstHeir].amount = (parseFloat(results[firstHeir].amount) + difference).toFixed(2);
+      const currentAmount = parseFloat(results[firstHeir].amount || 0);
+      results[firstHeir].amount = (currentAmount + difference).toFixed(2);
       results[firstHeir].percentage = ((parseFloat(results[firstHeir].amount) / totalAmount) * 100).toFixed(2);
     }
   }
